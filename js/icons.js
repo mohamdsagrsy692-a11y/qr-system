e74e66fb0d708dc26c7041146472f4f3220a23f6
mohamdsagrsy692-a11y <mohamdsagrsy692@gmail.com>
2026-08-19 07:21:20 +0300

    Add icons.js with multiple icon configurations

---
+++ js/icons.js
@@ -0,0 +1,66 @@
+{
+    "trash": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 60,
+        "width": 834,
+        "height": 946,
+        "layers": ["Слой 10", "Слой 9", "Слой 8", "Слой 7"]
+    },
+    "contacts": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 105,
+        "width": 856,
+        "height": 870,
+        "layers": ["Shape Mask", "Rotation Layer", "Position Path"]
+    },
+    "refresh": {
+        "version": "5.3.4",
+        "framerate": 48,
+        "inPoint": 0,
+        "outPoint": 86,
+        "width": 660,
+        "height": 615,
+        "layers": ["Loader", "Arrows", "Trim Paths 1"]
+    },
+    "download": {
+        "version": "5.3.4",
+        "framerate": 48,
+        "inPoint": 0,
+        "outPoint": 93,
+        "width": 410,
+        "height": 580,
+        "layers": ["Splashes", "Arrow", "Line Elements"]
+    },
+    "file_5": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 60,
+        "layers": ["Layer 1", "Layer 2"]
+    },
+    "file_6": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 60,
+        "layers": ["Layer 1", "Layer 2"]
+    },
+    "file_7": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 60,
+        "layers": ["Layer 1", "Layer 2"]
+    },
+    "file_8": {
+        "version": "5.3.4",
+        "framerate": 60,
+        "inPoint": 0,
+        "outPoint": 60,
+        "layers": ["Layer 1", "Layer 2"]
+    }
+}
